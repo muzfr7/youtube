33a7cfa09fb1f31ff9ac0f1294491a790a124526
Muzafar Ali <muzfr7@gmail.com>
2022-12-24 08:10:28 +0100

    Update user_repository comments

--- echoblog/part-7/repositories/user_repository.go
+++ echoblog/part-7/repositories/user_repository.go
@@ -5,7 +5,7 @@ import (
 	"github.com/muzfr7/echoblog/entities"
 )
 
-// UserRepository provide actions for manipulating users in the database.
+// UserRepository provides actions to manipulate users in the database.
 type UserRepository interface {
 	User(ID int) (entities.User, error)
 	Users() ([]entities.User, error)
@@ -14,33 +14,33 @@ type UserRepository interface {
 	Delete(ID int) error
 }
 
-// userRepo implements the UserRepository.
+// userRepo implements the UserRepository interface.
 type userRepo struct {
 	db *sqlx.DB
 }
 
-// NewUserRepo creates userRepo
+// NewUserRepo instantiates userRepo struct.
 func NewUserRepo(db *sqlx.DB) UserRepository {
 	return &userRepo{
 		db: db,
 	}
 }
 
-// User will fetch a user from the database and return it.
+// User fetches a user from the database and return it.
 func (r *userRepo) User(ID int) (entities.User, error) {
 	var user entities.User
 	err := r.db.Get(&user, "SELECT * FROM users WHERE id = $1", ID)
 	return user, err
 }
 
-// Users will fetch all the users from the database and return them.
+// Users fetches all the users from the database and return them.
 func (r *userRepo) Users() ([]entities.User, error) {
 	var users []entities.User
 	err := r.db.Select(&users, "SELECT * FROM users")
 	return users, err
 }
 
-// Insert will insert a new user into the database.
+// Insert inserts a new user into the database.
 func (r *userRepo) Insert(user entities.User) error {
 	_, err := r.db.NamedExec("INSERT INTO users (firstname, lastname, email, status) VALUES (:firstname, :lastname, :email, :status)", user)
 	if err != nil {
@@ -50,7 +50,7 @@ func (r *userRepo) Insert(user entities.User) error {
 	return nil
 }
 
-// Update will update give user in the database.
+// Update updates given user in the database.
 func (r *userRepo) Update(user entities.User) error {
 	_, err := r.db.NamedExec("UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email, status = :status) WHERE id = :id", user)
 	if err != nil {
@@ -60,7 +60,7 @@ func (r *userRepo) Update(user entities.User) error {
 	return nil
 }
 
-// Delete will delete a user from the database.
+// Delete removes a user from the database.
 func (r *userRepo) Delete(ID int) error {
 	_, err := r.db.Exec("DELETE FROM users WHERE id = $1", ID)
 	if err != nil {
